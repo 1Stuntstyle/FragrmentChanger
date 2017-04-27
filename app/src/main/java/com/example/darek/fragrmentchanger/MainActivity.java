3dea8b0d535c62e5f76d6d7e8a6dad25bd050728
Michal Idzkowski <michal.idzkowski@mobica.com>
2017-04-27 22:03:09 +0200

    Working fragment replacement

--- app/src/main/java/com/example/darek/fragrmentchanger/MainActivity.java
+++ app/src/main/java/com/example/darek/fragrmentchanger/MainActivity.java
@@ -1,48 +1,23 @@
 package com.example.darek.fragrmentchanger;
 
 import android.app.Fragment;
-import android.app.FragmentTransaction;
-import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
-
-    TextView tvSec;
-    Button bF1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bF1 = (Button) findViewById(R.id.button);
-        tvSec = (TextView) findViewById(R.id.tvSecond);
 
-        bF1.setOnClickListener(this);
+        final Fragment mainFragment = new MainFragment();
+        getFragmentManager().beginTransaction().add(R.id.container, mainFragment).commit();
     }
-
-
-
-
 
     @Override
     public void onClick(View v) {
 
-        //tvSec.setText("I'm second");
-/*
-        Fragment newFragment = new MainFragment();
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.replace(R.layout.first_fragment, newFragment);*/
-
-        Fragment secoundFragment = new SecondFragment();
-        FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
-        transaction1.remove(secoundFragment);
-     //   transaction.addToBackStack(null);
-
-// Commit the transaction
-        transaction1.commit();
     }
 }
