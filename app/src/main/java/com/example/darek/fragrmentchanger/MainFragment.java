fcc920f162ea88755fc664bcb7fb26b51cfc76dd
Michal Idzkowski <michal.idzkowski@mobica.com>
2017-05-08 21:32:52 +0200

    Remove unused elements

--- app/src/main/java/com/example/darek/fragrmentchanger/MainFragment.java
+++ app/src/main/java/com/example/darek/fragrmentchanger/MainFragment.java
@@ -8,38 +8,31 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-/**
- * Created by Darek on 2017-04-25.
- */
 public class MainFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.first_fragment, container, false);
 
-        Button button = (Button) view.findViewById(R.id.button);
-        Button button2 = (Button) view.findViewById(R.id.button2);
-        button.setOnClickListener(this);
-        button2.setOnClickListener(this);
+        Button bOpenSecondFragment = (Button) view.findViewById(R.id.bOpenSecondFragment);
+        Button bOpenThirdFragment = (Button) view.findViewById(R.id.bOpenThirdFragment);
+        bOpenSecondFragment.setOnClickListener(this);
+        bOpenThirdFragment.setOnClickListener(this);
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
-
-            case R.id.button:
+            case R.id.bOpenSecondFragment:
                 Fragment thirdFragment = new ThirdFragment();
                 FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
                 transaction1.replace(R.id.container, thirdFragment);
                 transaction1.addToBackStack(null);
                 transaction1.commit();
                 break;
-
-
-            case R.id.button2:
+            case R.id.bOpenThirdFragment:
                 Fragment secondFragment = new SecondFragment();
                 FragmentTransaction transaction2 = getFragmentManager().beginTransaction();
                 transaction2.replace(R.id.container, secondFragment);
